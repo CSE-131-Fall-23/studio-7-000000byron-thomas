e789ae47e5b47c34c0d6fd9439f35e8e104b0a16
byronwuh <byronwuh@localhost>
2023-11-02 11:20:11 -0500

    pls give 100 i tried my best

--- src/studio7/My_Little_Rectangle.java
+++ src/studio7/My_Little_Rectangle.java
@@ -1,5 +1,26 @@
 package studio7;
 
-public class My_Little_Rectangle {
+import java.io.FileNotFoundException;
 
+public class My_Little_Rectangle { //And the Power of Friendship
+	
+	public int width, length;
+	public My_Little_Rectangle (int l, int w) {
+		w = this.width;
+		l = this.length;
+
+	} 
+	public static int area(int l, int w) {
+		int area = (l * w);
+		return area;
+	}
+	
+	public static int per(int l, int w) {
+		int per = (2*l + 2*w);
+		return per;
+	}
+	
+	public static void main(String[] args) {
+		return My_Little_Rectangle(l,w);
+	}
 }
